2ce3039177c0cb9ff15ad39a99f66a54ef24c1bc
Coolboyzlol99 <avyangupta1212@gmail.com>
2026-06-13 14:52:25 +0530

    In this program I have used functions to give well wishes to the user

---
+++ main.py
@@ -0,0 +1,8 @@
+#Starting print
+print("In this program I have used functions to give well wishes to the user.")
+
+#Defining function
+def wishes():
+       print("Hello, how are you.")
+#Calling a function
+wishes()
